fc5915b43699d4c64ed52bb880b9d99193b17f3d
Nassim <nassim19961996@outlook.fr>
2019-01-15 16:59:49 +0100

    AjoutEntity: Compte + Autoloader

--- service/autoloader.php
+++ service/autoloader.php
@@ -3,7 +3,7 @@
 //On utilise principalement les constantes éviter l'instanciation d'un objet
 class autoloader {
   //On enregistre dans des constantes de classe les entités, les services et les managers
-  const entity = ["entity", "user"];
+  const entity = ["entity", "user", "Compte"];
   const service= ["autoloader"];
   const manager = ["dataBase","manager"];
   //Fonction qui appelle l'autoload register qui se base sur la fonction statique loader
