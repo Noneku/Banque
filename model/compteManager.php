11b42b513f4b306f32f8c3e351b572290d38e4ea
Votre Nom <Vous@exemple.com>
2019-01-16 11:31:06 +0100

    compteManager.php coded

---
+++ model/compteManager.php
@@ -0,0 +1,51 @@
+<?php
+abstract class compte extends manager
+{
+
+//recupérer touts les comptes.
+  public function getComptes(){
+    $query = $this->getDb()->query('SELECT * FROM compte');
+    $data = $query->fetchAll(PDO::FETCH_CLASS, "Compte");
+    return $data;
+    var_dump($data);
+  }
+
+//ajouter un nouveau compte
+
+public function addCompte($comptes) {
+  var_dump($comptes);
+  $request = $db->prepare("INSERT INTO Copmte (nom, somme) VALUES(:nom, :somme)");
+  $result = $request->execute([
+      "nom" => $user["nom"] ,
+      "somme" => $user["somme"]
+  ]);
+  $request->closeCursor();
+  return $result;
+}
+
+//modifier un comptes
+
+function updateCompte($id) {
+    $request = $db->prepare("UPDATE Compte SET nom = :somme WHERE id = :id");
+    $result = $request->execute([
+        "nom" => $user["nom"] ,
+        "somme" => $user["somme"]
+        "id" => $id
+    ]);
+    $request->closeCursor();
+    return $result;
+}
+
+//supprimer un comptes
+function deleteCompte($id) {
+    $request = $db->prepare("DELETE FROM Compte WHERE id = ?");
+    $request->execute([$id]);
+    $result = $request->fetch(PDO::FETCH_ASSOC);
+    $request->closeCursor();
+    return $result;
+}
+
+
+}
+
+?>
