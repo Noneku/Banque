e93e1c94666179282c5ae7c84cab77e8d441254e
Votre Nom <Vous@exemple.com>
2019-01-16 16:11:39 +0100

    to push

--- model/compteManager.php
+++ model/compteManager.php
@@ -1,8 +1,8 @@
 <?php
-abstract class compte extends manager
+class compteManager extends manager
 {
 
-//recupérer touts les comptes.
+  //recupérer touts les comptes.
   public function getComptes(){
     $query = $this->getDb()->query('SELECT * FROM compte');
     $data = $query->fetchAll(PDO::FETCH_CLASS, "Compte");
@@ -10,40 +10,40 @@ abstract class compte extends manager
     var_dump($data);
   }
 
-//ajouter un nouveau compte
+  //ajouter un nouveau compte
 
-public function addCompte($comptes) {
-  var_dump($comptes);
-  $request = $db->prepare("INSERT INTO Copmte (nom, somme) VALUES(:nom, :somme)");
-  $result = $request->execute([
+  public function addCompte($comptes) {
+    var_dump($comptes);
+    $request = $db->prepare("INSERT INTO Copmte (nom, somme) VALUES(:nom, :somme)");
+    $result = $request->execute([
       "nom" => $user["nom"] ,
       "somme" => $user["somme"]
-  ]);
-  $request->closeCursor();
-  return $result;
-}
-
-//modifier un comptes
-
-function updateCompte($id) {
-    $request = $db->prepare("UPDATE Compte SET nom = :somme WHERE id = :id");
-    $result = $request->execute([
-        "nom" => $user["nom"] ,
-        "somme" => $user["somme"]
-        "id" => $id
     ]);
     $request->closeCursor();
     return $result;
-}
+  }
 
-//supprimer un comptes
-function deleteCompte($id) {
+  //modifier un comptes
+
+  function updateCompte($id) {
+    $request = $db->prepare("UPDATE Compte SET nom = :somme WHERE id = :id");
+    $result = $request->execute([
+      "nom" => $user["nom"] ,
+      "somme" => $user["somme"]
+      "id" => $id
+    ]);
+    $request->closeCursor();
+    return $result;
+  }
+
+  //supprimer un comptes
+  function deleteCompte($id) {
     $request = $db->prepare("DELETE FROM Compte WHERE id = ?");
     $request->execute([$id]);
     $result = $request->fetch(PDO::FETCH_ASSOC);
     $request->closeCursor();
     return $result;
-}
+  }
 
 
 }
--- model/dataBase.php
+++ model/dataBase.php
@@ -3,10 +3,10 @@
  * Class to connect to the data base
  */
 class dataBase {
-  const host  = "";
-  const dbName = "";
-  const login = "";
-  const mdp = "";
+  const host  = "loalhost";
+  const dbName = "Banque";
+  const login = "phpmyadmin";
+  const mdp = "Manzakin2603";
   static public function BD() {
     $db = new PDO("mysql:host=" . self::host .";dbname=" . self::dbName , self::login, self::mdp);
     return $db;
